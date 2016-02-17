4b00ea22098de3a8cc713224fa54eadc2388f014
bmcAD <kavraham@bmc.com>
2016-02-17 13:58:57 +0200

    Update windowsOS.lua

--- modules/windowsOS.lua
+++ modules/windowsOS.lua
@@ -6,7 +6,7 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-local _helper = require('./pluginHelper')
+local _helper = require('pluginHelper')
 
 
 local windowsOS = {}
@@ -77,3 +77,5 @@ end
 windowsOS.execute = execute
 
 return windowsOS
+
+
